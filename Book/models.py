2292e6e153ac7d3085027ee277b7e3ca0c2f294e
Egor Bashkirov <EgorBaschckirov@yandex.ru>
2022-12-26 14:16:20 +0300

    новый файл:    .gitignore 	изменено:      Book/admin.py 	новый файл:    Book/forms.py 	новый файл:    Book/migrations/0001_initial.py 	изменено:      Book/models.py 	новый файл:    Book/urls.py 	изменено:      Book/views.py 	изменено:      Library/urls.py 	изменено:      db.sqlite3

--- Book/models.py
+++ Book/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
